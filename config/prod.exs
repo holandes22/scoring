b4c9452e656a74f2d9b24cfd939270cb54796f32
Pablo Klijnjan <pabloklijnjan@gmail.com>
2017-07-06 15:58:28 +0300

    Fixed prod config

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :scoring, Scoring.Web.Endpoint,
   on_init: {Scoring.Web.Endpoint, :load_from_system_env, []},
-  url: [host: "radiant-earth-11117.herokuapp.com", port: 443],
+  url: [host: "twist-scoring.herokuapp.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
